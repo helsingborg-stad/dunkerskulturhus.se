c0118cea5cc85b2b7a0ec7e0a784b876d97ad913
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2018-07-16 07:44:28 +0200

    restyle

--- wp-content/themes/dunkerskulturhus/assets/dist/js/app.js
+++ wp-content/themes/dunkerskulturhus/assets/dist/js/app.js
@@ -97,26 +97,26 @@ DunkersKultur.ScrollPlease = DunkersKultur.ScrollPlease || {};
 DunkersKultur.ScrollPlease.ScrollPlease = (function ($) {
 
     function ScrollPlease() {
-        $(window).on('scroll', function (e) {
-            var scrollPos = $(e.target).scrollTop();
+        // $(window).on('scroll', function (e) {
+        //     var scrollPos = $(e.target).scrollTop();
 
-            if (scrollPos > 100) {
-                $('.scroll-down-please').fadeOut();
-            } else {
-                $('.scroll-down-please').fadeIn();
-            }
-        });
+        //     if (scrollPos > 100) {
+        //         $('.scroll-down-please').fadeOut();
+        //     } else {
+        //         $('.scroll-down-please').fadeIn();
+        //     }
+        // });
 
-        $('.scroll-down-please').on('click', function (e) {
-            var target = $(e.target).closest('a').attr('href');
-            var scrollTo = $(target).offset().top - $('.navbar-mainmenu').height();
+        // $('.scroll-down-please').on('click', function (e) {
+        //     var target = $(e.target).closest('a').attr('href');
+        //     var scrollTo = $(target).offset().top - $('.navbar-mainmenu').height();
 
-            $('html, body').animate({
-                scrollTop: scrollTo
-            }, 500);
+        //     $('html, body').animate({
+        //         scrollTop: scrollTo
+        //     }, 500);
 
-            return false;
-        });
+        //     return false;
+        // });
     }
 
     return new ScrollPlease();
