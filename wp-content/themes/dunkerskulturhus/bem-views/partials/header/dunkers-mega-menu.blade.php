70583efd6c24adb5f1bbb7f366d941e46026520e
nRamstedt <nikolas.ramstedt@gmail.com>
2020-09-11 11:52:47 +0200

    Add mega menu view (duplicate of old menu)

---
+++ wp-content/themes/dunkerskulturhus/bem-views/partials/header/dunkers-mega-menu.blade.php
@@ -0,0 +1,25 @@
+@extends('partials.header.default')
+
+@section('header-body')
+    <nav class="navbar navbar-mainmenu">
+        <div class="container">
+            <div class="grid">
+                <div class="grid-xs-12 {!! apply_filters('Municipio/header_grid_size','grid-md-12'); !!}">
+
+                    {!! municipio_get_logotype(get_field('header_logotype', 'option'), get_field('logotype_tooltip', 'option')) !!}
+
+                    {!! $navigation['mainMenu'] !!}
+                    <a href="#mobile-menu" data-target="#mobile-menu" class="{!! apply_filters('Municipio/mobile_menu_breakpoint','hidden-md hidden-lg'); !!} menu-trigger"><span class="menu-icon"></span></a>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+    @if (strlen($navigation['mobileMenu']) > 0)
+        <nav id="mobile-menu" class="nav-mobile-menu nav-toggle {!! apply_filters('Municipio/mobile_menu_breakpoint','hidden-md hidden-lg'); !!}">
+            <div class="container u-p-0">
+                @include('partials.mobile-menu')
+            </div>
+        </nav>
+    @endif
+@stop
