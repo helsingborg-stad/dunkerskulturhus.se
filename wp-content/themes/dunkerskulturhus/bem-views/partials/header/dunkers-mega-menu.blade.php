9704fd955010498728423a06b977ea72852b07a6
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2020-09-17 14:55:14 +0200

    Fix: Always keep logo visible

--- wp-content/themes/dunkerskulturhus/bem-views/partials/header/dunkers-mega-menu.blade.php
+++ wp-content/themes/dunkerskulturhus/bem-views/partials/header/dunkers-mega-menu.blade.php
@@ -1,7 +1,7 @@
 @extends('partials.header.default')
 
 @section('header-body')
-    <nav class="navbar navbar-mainmenu">
+    <nav class="navbar navbar-mainmenu navbar-mainmenu--mega">
         <div class="container">
             <div class="grid">
                 <div class="grid-xs-12 {!! apply_filters('Municipio/header_grid_size','grid-md-12'); !!}">
@@ -9,7 +9,7 @@
                     {!! municipio_get_logotype(get_field('header_logotype', 'option'), get_field('logotype_tooltip', 'option')) !!}
 
                     {{-- {!! $navigation['mainMenu'] !!} --}}
-                    <a href="#mobile-menu" data-target="#mobile-menu" class="menu-trigger"><span class="menu-icon"></span></a>
+                    <a href="#mobile-menu" data-target="#mobile-menu" class="menu-trigger"><span class="menu-trigger__label">Meny</span> <span class="menu-icon"></span></a>
                 </div>
             </div>
         </div>
