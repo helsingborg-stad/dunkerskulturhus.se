b4eeeb6f5eefeb1a200c3b1c49d3864cbe10a3fe
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2020-09-28 09:39:05 +0200

    Fix: Add translate button to mega menu

--- wp-content/themes/dunkerskulturhus/bem-views/partials/header/dunkers-mega-menu.blade.php
+++ wp-content/themes/dunkerskulturhus/bem-views/partials/header/dunkers-mega-menu.blade.php
@@ -4,11 +4,15 @@
     <nav class="navbar navbar-mainmenu navbar-mainmenu--mega">
         <div class="container">
             <div class="grid">
-                <div class="grid-xs-12 {!! apply_filters('Municipio/header_grid_size','grid-md-12'); !!}">
+                <div class="grid-xs-12 {!! apply_filters('Municipio/header_grid_size','grid-md-12'); !!} u-justify-content-start">
 
-                    {!! municipio_get_logotype(get_field('header_logotype', 'option'), get_field('logotype_tooltip', 'option')) !!}
+                    <span class="u-mr-auto">    
+                        {!! municipio_get_logotype(get_field('header_logotype', 'option'), get_field('logotype_tooltip', 'option')) !!}
+                    </span>
 
-                    {{-- {!! $navigation['mainMenu'] !!} --}}
+                    {{-- Translate --}}
+                    <a href="?translate=true#translate" class="translate-icon-btn u-mr-2" aria-label="translate"><span data-tooltip="Translate"><i class="pricon pricon-globe"></i></span></a>
+
                     <a href="#mobile-menu" role="button" tabindex="0" data-target="#mobile-menu" class="menu-trigger"><span class="menu-trigger__label">Meny</span><span class="menu-trigger__close">Stäng</span><span class="menu-icon"></span></a>
                 </div>
             </div>
